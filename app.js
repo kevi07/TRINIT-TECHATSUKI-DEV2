68df9528909682b7ce52f3c3e7544608b5de9039
kar156505@gmail.com <kar156505@gmail.com>
2024-03-10 02:17:50 +0530

    .

--- app.js
+++ app.js
@@ -1,7 +1,71 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
 const path = require("path");
+
+
+mongoose.connect('mongodb+srv://kevin:Kevin123@cluster0.fdzpbea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const tutorSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    experience: String,
+    history: String,
+    languages: [String],
+    phoneNumber: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    pricing: Number,
+    timings: [String],
+  });
+  
+
+  const Tutor = mongoose.model('Tutor', tutorSchema);
+  
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  
+  app.post('/tutor/register', async (req, res) => {
+    try {
+      const {
+        name,
+        age,
+        experience,
+        history,
+        languages,
+        phoneNumber,
+        email,
+        pricing,
+        timings,
+      } = req.body;
+  
+      const tutor = new Tutor({
+        name,
+        age,
+        experience,
+        history,
+        languages,
+        phoneNumber,
+        email,
+        pricing,
+        timings,
+      });
+  
+      const savedTutor = await tutor.save();
+      res.status(201).json(savedTutor);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
 
 app.set('view engine' , 'ejs');
 
@@ -27,7 +91,7 @@ app.get('/tutorworkspace' , (req,res) => {
 
 app.get('/videoplayer', (req, res) => { 
     const range = req.headers.range 
-    const videoPath = './video/English_course.mp4'; 
+    const videoPath = '/video/English_course.mp4'; 
     const videoSize = fs.statSync(videoPath).size 
     const chunkSize = 1 * 1e6; 
     const start = Number(range.replace(/\D/g, "")) 
