4a4b3b302e54cabd374cf67feedb2930612bf2a0
kar156505@gmail.com <kar156505@gmail.com>
2024-03-10 02:03:03 +0530

    .

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const path = require("path");
 
 app.set('view engine' , 'ejs');
@@ -22,6 +23,28 @@ app.get('/tutor', (req, res) => {
 
 app.get('/tutorworkspace' , (req,res) => {
     res.render('tutor')
-})
+});
+
+app.get('/videoplayer', (req, res) => { 
+    const range = req.headers.range 
+    const videoPath = './video/English_course.mp4'; 
+    const videoSize = fs.statSync(videoPath).size 
+    const chunkSize = 1 * 1e6; 
+    const start = Number(range.replace(/\D/g, "")) 
+    const end = Math.min(start + chunkSize, videoSize - 1) 
+    const contentLength = end - start + 1; 
+    const headers = { 
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`, 
+        "Accept-Ranges": "bytes", 
+        "Content-Length": contentLength, 
+        "Content-Type": "video/mp4"
+    } 
+    res.writeHead(206, headers) 
+    const stream = fs.createReadStream(videoPath, { 
+        start, 
+        end 
+    }) 
+    stream.pipe(res) 
+}) 
 
 app.listen(5000);
